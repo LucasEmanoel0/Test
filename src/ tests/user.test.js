12f6444cd836339fc9bcae099f0992a9643eae06
LucasEmanoel0 <lcsdeidara@gmail.com>
2024-07-28 20:01:37 -0300

    feat:teste da rota post /criar

--- src/ tests/user.test.js
+++ src/ tests/user.test.js
@@ -3,20 +3,24 @@ import app from '../index.js'
 
 describe("POSTT /user",()=>{
     it('criando dados no bd',async ()=>{
+
         let user = {
-            name:"lucas",
+            nome:"lucas",
+            senha:"lucasemanoel",
             email:"lucas@gmail.com",
-            password:"lucasemanoel",
             dataNascimento:"2002-12-25"
         }
-            let test = await supertest(app).post('/user').send(user).expect(201).expect((res)=>{
-                if(res.body.message  !== "Usuário criado com sucesso"){
-                    throw new Error(`Expected message "Usuário criado com sucesso" but got "${res.body.message}"`);
-                }else{
-                    console.log("usuario criado com sucesso")
-                }
+
+           try {
+            await supertest(app).post('/criar').send(user).expect(201).expect(res=>{
+                let {message} = res.body
+                console.log(message)
+                expect(message).toBe("Usuario criado com sucesso")
             })
-        return test
+           } catch (error) {
+            console.log(error)
+            
+           }
         
     })
 })
