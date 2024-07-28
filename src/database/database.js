caca416fd1e2fa2d294c7f6839cc58cee01f355b
LucasEmanoel0 <lcsdeidara@gmail.com>
2024-07-28 20:03:58 -0300

    wip:configurar as envs do bd

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,13 @@
 import pg from 'pg'
 const {Pool} = pg
 
-const pool  = new Pool({
-    database: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_USER,
+const pool = new Pool({
+    user: 'postgres',
+    password: 'itachi007',
+    host: 'localhost',
     port: 5432,
-})
+    database: 'postgres',
+  })
 
 export default pool
 
