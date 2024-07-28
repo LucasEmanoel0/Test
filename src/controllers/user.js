c1dfa18ef04ed1aa22edee21855725a33a1e2bc9
ItzTas <talessabini2@gmail.com>
2024-07-28 17:30:54 -0300

    fix: lenght check added

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,25 +1,23 @@
-
-import pool from "../database/database"
-
+import pool from "../database/database";
 
 export default {
-    newCount: async (req,res)=>{
-        let {nome,senha,email,dataNascimento} = req.body
-        try {
-            let result = await pool.query('INSERT INTO USER (name,email,password,dataNascimento) VALUES ($1,$2, $3, $4)',[nome,senha,email,dataNascimento])
+  newCount: async (req, res) => {
+    let { nome, senha, email, dataNascimento } = req.body;
+    try {
+      let result = await pool.query(
+        "INSERT INTO USER (name,email,password,dataNascimento) VALUES ($1,$2, $3, $4)",
+        [nome, senha, email, dataNascimento]
+      );
 
-            if(result.rows > 0){
-                res.status(201).send({message:"usuario criado com sucesso"})
-                console.log(result)
-            }else{
-                res.status(500).send({message:"erro ao criar usuario"})
-            }
-            
-        } catch (error) {
-            console.log(error)
-            res.status(500).send({message:"error"})
-            
-        }  
-
+      if (result.rows.lenght > 0) {
+        res.status(201).send({ message: "usuario criado com sucesso" });
+        console.log(result);
+      } else {
+        res.status(500).send({ message: "erro ao criar usuario" });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "error" });
     }
-}
+  },
+};
