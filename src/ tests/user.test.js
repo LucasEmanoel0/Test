5d6ecf133b961ca751507ca0cf7a907006b6e100
LucasEmanoel0 <lcsdeidara@gmail.com>
2024-07-29 11:14:42 -0300

    feat:add test de dados vazios no bd

--- src/ tests/user.test.js
+++ src/ tests/user.test.js
@@ -5,7 +5,7 @@ describe("POSTT /user",()=>{
     it('criando dados no bd',async ()=>{
 
         let user = {
-            nome:"lucas",
+            nome:"davi",
             senha:"lucasemanoel",
             email:"lucas@gmail.com",
             dataNascimento:"2002-12-25"
@@ -23,6 +23,28 @@ describe("POSTT /user",()=>{
            }
         
     })
+
+    it('recusando dados vazios',async ()=>{
+
+        let user = {
+            nome:"",
+            senha:"lucasemanoel",
+            email:"",
+            dataNascimento:"2002-12-25"
+        }
+           try {
+            await supertest(app).post('/criar').send(user).expect(400).expect(res=>{
+                let {message} = res.body
+                console.log(message)
+                expect(message).toBe("Preencha todos os dados")
+            })
+           } catch (error) {
+            console.log(error)
+            
+           }
+        
+    })
+
 })
 
 
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,20 +5,28 @@ import pool from "../database/database.js"
 export default {
     newCount: async (req,res)=>{
         let {nome,senha,email,dataNascimento} = req.body
-        try {
-            let result = await pool.query('INSERT INTO "TEST" ("name","email","password","dataNascimento") VALUES ($1,$2, $3, $4)',[nome,email,senha,dataNascimento])
 
-            if(result.rowCount > 0){
-                res.status(201).send({message:"Usuario criado com sucesso"})
-            }else{
-                res.status(500).send({message:"erro ao criar usuario"})
-            }
+        if(!nome || !senha || !email || !dataNascimento){
+            res.status(400).send({message:"Preencha todos os dados"})
             
-        } catch (error) {
-            console.log("error ao inserir no banco de dados" + error)
-            res.status(500).send(`erro : ${error.message}`)
-            
-        }  
+        }else{
+            try {
+                let result = await pool.query('INSERT INTO "TEST" ("name","email","password","dataNascimento") VALUES ($1,$2, $3, $4)',[nome,email,senha,dataNascimento])
+    
+                if(result.rowCount > 0){
+                    res.status(201).send({message:"Usuario criado com sucesso"})
+                }else{
+                    res.status(500).send({message:"erro ao criar usuario"})
+                }
+                
+            } catch (error) {
+                console.log("error ao inserir no banco de dados" + error)
+                res.status(500).send(`erro : ${error.message}`)
+                
+            }  
 
+        }
+
+        
     }
 }
