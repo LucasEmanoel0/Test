a3170b31e5acb1e12a08824662bdaf0041daec97
LucasEmanoel0 <lcsdeidara@gmail.com>
2024-07-29 11:57:59 -0300

    feat:add teste da rota /login

--- src/ tests/user.test.js
+++ src/ tests/user.test.js
@@ -1,7 +1,7 @@
 import supertest from 'supertest'
 import app from '../index.js'
 
-describe("POSTT /user",()=>{
+describe("POST /criar",()=>{
     it('criando dados no bd',async ()=>{
 
         let user = {
@@ -45,6 +45,77 @@ describe("POSTT /user",()=>{
         
     })
 
+})
+
+describe("POST  /login",()=>{
+
+    it('Login de usuario feito com sucesso',async ()=>{
+        let user = {
+            senha:"lucasemanoel",
+            email:"lucas@gmail.com"
+        }
+
+        try {
+            await supertest(app).post('/login')
+            .send(user)
+            .expect(200)
+            .expect((res)=>{
+                let {message} = res.body
+                console.log(message)
+                expect(message).toBe("Usuario autenticado")
+               })
+            
+        } catch (error) {
+            console.log(`error é ${error} `)
+            throw error 
+            
+        }
+
+    })
+    it('Login não encontrado',async ()=>{
+        let user = {
+            senha:"lucasemanoel",
+            email:"rafael@gmail.com"
+        }
+        try {
+            await supertest(app).post('/criar').send(user).expect(404).expect((res)=>{
+                let {message} = res.body
+                console.log(message)
+                describe(message).toBe("Usuario não encontrado")
+            })
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+        
+    })
+    it('Dados nao preenchidos',async ()=>{
+        let user = {
+            senha:"lucasemanoel",
+            email:""
+        }
+        try {
+            await supertest(app).post('/criar').send(user).expect(400).expect((res)=>{
+                let {message} = res.body
+                console.log(message)
+                describe(message).toBe("Email e senha são obrigatarios")
+            })
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+        
+    })
+
+})
+
+describe("PATCH /user",()=>{
+    it('senha atualizada com sucesso',async ()=>{
+        
+
+    })
 })
 
 
